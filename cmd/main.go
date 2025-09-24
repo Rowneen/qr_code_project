5b2843eebda0fa848cd408c6a127e2e8e5467dad
yungmilonov <unuandina@gmail.com>
2025-09-25 02:08:41 +0400

    rename http_handler package name 2

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"qr_code/internal/config"
-	http_handlers "qr_code/internal/handlers"
+	http_handler "qr_code/internal/handlers"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -43,5 +43,5 @@ func main() {
 	log.Println("SUCCESS: Connected to database!")
 	fmt.Println("Also printing via fmt:", cfg)
 
-	http_handlers.RegisterHandlers()
+	http_handler.RegisterHandlers()
 }
