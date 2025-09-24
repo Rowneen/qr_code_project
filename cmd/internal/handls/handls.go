fff84e4ff0c2b653b4b5d5a6f06a1195c035f4fe
Rowneen <157363931+Rowneen@users.noreply.github.com>
2025-09-25 00:42:05 +0300

    new

---
+++ cmd/internal/handls/handls.go
@@ -0,0 +1,24 @@
+package handls
+
+import (
+	"io"
+	"net/http"
+)
+
+// сами хандлеры обработчики
+func handler_auth(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Write([]byte("only post"))
+		return
+	}
+	body, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	w.Write([]byte(string(body)))
+}
+
+// регистрация хандлеров и запуск
+func RegisterHandlers() {
+	http.HandleFunc("/auth", handler_auth)
+	http.ListenAndServe(":8080", nil)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	"fmt"
-	_ "net/http"
-	"qr_code/internal/config"
-	"log"
 	"database/sql"
+	"fmt"
+	"log"
+	_ "net/http"
+	"os"
+	"path/filepath"
+	"qr_code/internal/config"
+	"qr_code/internal/handls"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var cfg = config.MustLoad()
 
 func main() {
-	cfg := config.MustLoad()
-	db, err := sql.Open("sqlite3", "./db/sqlite.db")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	currentDir, err := os.Getwd()
 
+	if err != nil {
+		log.Fatalf("ошибко пути: %s", err)
+	}
 
+	dbPath := filepath.Clean(filepath.Join(currentDir, "db", cfg.StoragePath))
 
-	log.Println("Successfully connected to database!")
+	log.Println("Starting application...")
+	log.Printf("Config: %+v\n", cfg)
 
-	fmt.Println(cfg)
-	
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal("SQL Open error:", err)
+	}
+	defer db.Close()
 
+	log.Println("Database opened, pinging...")
 
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Ping failed:", err)
+	}
+
+	log.Println("SUCCESS: Connected to database!")
+	fmt.Println("Also printing via fmt:", cfg)
+
+	handls.RegisterHandlers()
 }
