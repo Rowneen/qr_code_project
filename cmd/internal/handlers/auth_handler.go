4b7426eda83915568c6690710d2625e7dc5b152a
yungmilonov <geronezqq@gmail.com>
2025-09-30 23:01:37 +0400

    cors headers

--- cmd/internal/handlers/auth_handler.go
+++ cmd/internal/handlers/auth_handler.go
@@ -27,6 +27,15 @@ type AuthResponse struct {
 
 func handler_auth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 200
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != "POST" {
 		response := AuthResponse{
 			Success: false,
--- cmd/internal/handlers/lesson_handler.go
+++ cmd/internal/handlers/lesson_handler.go
@@ -28,6 +28,15 @@ type LessonResponse struct {
 
 func handler_lesson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 200
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if r.Method != "POST" {
 		response := AuthResponse{
 			Success: false,
