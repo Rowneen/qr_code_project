fa53b0b22329d0f30a41f6a4e70c2414cc1e47ee
yungmilonov <geronezqq@gmail.com>
2025-12-08 23:48:01 +0400

    regex fix

--- cmd/internal/utils/string_utils.go
+++ cmd/internal/utils/string_utils.go
@@ -5,10 +5,10 @@ import (
 )
 
 func IsSafeString(input string) bool {
-	matched, _ := regexp.MatchString(`^[a-zA-Z0-9@.\-_]{3,50}$`, input)
+	matched, _ := regexp.MatchString(`^[\p{Cyrillic}a-zA-Z0-9@.\-_]{3,50}$`, input)
 	return matched
 }
 
 func CleanString(input string) string {
-	return regexp.MustCompile(`[^a-zA-Z0-9@.\-_]`).ReplaceAllString(input, "")
+	return regexp.MustCompile(`[^\p{Cyrillic}a-zA-Z0-9@.\-_]`).ReplaceAllString(input, "")
 }
